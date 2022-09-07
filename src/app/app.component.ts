20eeef07ec1f3a0335d1d86f8585a08af41767eb
Vladi <vladi@notifyai.io>
2022-09-07 16:33:04 +0300

    firs working version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { parse as csvParse } from 'papaparse';
+import {vocabulary} from "./vocabulary";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calcalculator';
+  title = 'calCalculator';
+  rows?: string[][];
+
+  onUpload(target: any) {
+    const file = (target as HTMLInputElement).files?.item(0);
+
+    if (file) csvParse(file as any, {
+      dynamicTyping: false,
+      step: undefined,
+      complete: results => {
+        console.log("Parsing complete:", results);
+        if (results.data.length) this.processData(results.data as any);
+        else console.error('Empty csv file provided');
+      },
+      skipEmptyLines: true,
+      fastMode: undefined,
+      transform: undefined,
+    })
+  }
+
+  processData(data: string[][]) {
+    // if (String(results.data[0]).includes('פירוט')) console.log('file format is correct');
+
+    if (data[0].length === 1) {
+      data.pop();
+      data.shift();
+      data.shift();
+      data.shift();
+    }
+
+    data.forEach(line => {
+      if (line.length < 5) line.push('');
+      const translationItem = vocabulary.find(item => line[1].includes(item.keyword));
+      if (translationItem) line.push(translationItem.translation)
+    })
+
+    this.rows = data;
+  }
+
+
+
 }
---
+++ src/app/vocabulary.ts
@@ -0,0 +1,320 @@
+interface IPayment {
+  keyword: string;
+  translation: string;
+  category?: string;
+  logo? :string;
+}
+
+export const vocabulary: IPayment[] = [
+  {
+    "keyword": "איקאה-מסעדה",
+    "translation": "Ikea restaurant",
+    category: 'food'
+  },
+  {
+    "keyword": "איקאה",
+    "translation": "Ikea"
+  },
+  {
+    "keyword": "אנאל מרקט",
+    "translation": "Supermarket",
+    category: 'food'
+  },  {
+    "keyword": "שניצל קומפני",
+    "translation": "Schnitzel Company",
+    category: 'food'
+  },  {
+    "keyword": "גומבה",
+    "translation": "Goomba cafe",
+    category: 'food'
+  },  {
+    "keyword": "שומשום",
+    "translation": "SumSum cafe",
+    category: 'food'
+  },  {
+    "keyword": "איבנטבאז כרטיסים",
+    "translation": "Concert tickets",
+  },
+  {
+    "keyword": "דליקטסן",
+    "translation": "Delicatessen",
+    category: 'food'
+  },  {
+    "keyword": "פקניק פארק הירקון",
+    "translation": "Picnic Cafe Park Yarkon",
+    category: 'food'
+  },
+  {
+    "keyword": "דמי כרטיס",
+    "translation": "Card payment"
+  },
+  {
+    "keyword": "ויקטורי ",
+    "translation": "Victory",
+    category: 'food'
+  },
+  {
+    "keyword": "זוהר בייקר",
+    "translation": "Zohar bakery",
+    category: 'food'
+  },
+  {
+    "keyword": "טיב טעם",
+    "translation": "Tiv Tam",
+    category: 'food'
+  },
+  {
+    "keyword": "לחם ארז",
+    "translation": "Lehem",
+    category: 'food'
+  },
+  {
+    "keyword": "מסעדת קוקו רמת גן",
+    "translation": "Kuku cafe",
+    category: 'food'
+  },
+  {
+    "keyword": "סונול צומת השרון נתנ",
+    "translation": "Sonol Gas Station",
+    category: 'car'
+  },
+  {
+    "keyword": "סופר פארם",
+    "translation": "SuperPharm",
+    category: 'health'
+  },
+  {
+    "keyword": "סופר קופיקס",
+    "translation": "Super Cofix",
+    category: 'food'
+  },
+  {
+    "keyword": "סיטי מרקט",
+    "translation": "City market",
+    category: 'food'
+  },
+  {
+    "keyword": "פז / YELLOWI עילית",
+    "translation": "Yellow gas station",
+    category: 'car'
+  },
+  {
+    "keyword": "שופרסל",
+    "translation": "Shufersal",
+    category: 'food'
+  },
+  {
+    "keyword": "תן ביס",
+    "translation": "10bis"
+  },
+  {
+    "keyword": "קשת טעמים נתניה פולג",
+    "translation": "Keshet taamim",
+    category: 'food'
+  },
+  {
+    "keyword": "נון סטופ מרקט",
+    "translation": "Not Stop Supermarket",
+    category: 'food'
+  },
+  {
+    "keyword": "LIM*",
+    "translation": "Lime Rent"
+  },
+  {
+    "keyword": "כלבו יחזקאל",
+    "translation": "Construction/electric store"
+  },
+  {
+    "keyword": "מינימרקט המרכז",
+    "translation": "Mini-market",
+    category: 'food'
+  },
+  {
+    "keyword": "העברה ב BIT",
+    "translation": "Bit"
+  },  {
+    "keyword": "AMZN",
+    "translation": "Amazon"
+  },
+  {
+    "keyword": "פיצה עגבניה דיזינגוף",
+    "translation": "Pizza Dizingoff Center",
+    category: 'food'
+  },
+  {
+    "keyword": "קפה נואר",
+    "translation": "Cafe Noar",
+    category: 'food'
+  },
+  {
+    "keyword": "קפה קפה",
+    "translation": "Cafe-Cafe",
+    category: 'food'
+  },
+  {
+    "keyword": "סלופארק טכנולוגיות בעמ",
+    "translation": "Parking",
+    category: 'car'
+  },
+  {
+    "keyword": "מכון הדרום",
+    "translation": "Car test",
+    category: 'car'
+  },
+  {
+    "keyword": "טמבור לילנבלום",
+    "translation": "Construction store near office"
+  },
+  {
+    "keyword": "חניון מגדל אלקטרה",
+    "translation": "Parking Maccabi",
+    category: 'car'
+  },
+  {
+    "keyword": "מכבי פארם תא השלום",
+    "translation": "Maccabi Pharm",
+    category: 'health'
+  },
+  {
+    "keyword": "עירית רמת גן אינטרנט",
+    "translation": "Arnona Ramat Gan"
+  },
+  {
+    "keyword": "דלק המפרץ",
+    "translation": "Gas station",
+    category: 'car'
+  },
+  {
+    "keyword": "יינות וטעמים",
+    "translation": "Wine & Cheese",
+    category: 'food'
+  },
+  {
+    "keyword": "ביטוח כללי מנורה מבטחים",
+    "translation": "Car Insurance",
+    category: 'car'
+  },
+  {
+    "keyword": "מלכה מרקט מגדל אביב",
+    "translation": "Minimarket",
+    category: 'food'
+  },
+  {
+    "keyword": "מגה בעיר קניון גבעתיים",
+    "translation": "Supermarket in Canyon Givataim",
+    category: 'food'
+  },
+  {
+    "keyword": "מקס רמת גן",
+    "translation": "Max 20 Ramat Gan"
+  },
+  {
+    "keyword": "ש. שלמה חברה לביטוח בעמ",
+    "translation": "Car Insurance",
+    category: 'car'
+  },
+  {
+    "keyword": "שטף קצף",
+    "translation": "Car Wash",
+    category: 'car'
+  },
+  {
+    "keyword": "יאלי בשכונה",
+    "translation": "Minimarket",
+    category: 'food'
+  },
+  {
+    "keyword": "חברת החשמל לישראל בעמ",
+    "translation": "Electricity company"
+  },
+  {
+    "keyword": "אושר עד תל אביב",
+    "translation": "Osher Ad",
+    category: 'food'
+  },
+  {
+    "keyword": "חנות המפעל מחלבת דשן",
+    "translation": "Cheese and Bread shop",
+    category: 'food'
+  },
+  {
+    "keyword": "נכסי אריאל-חניון מרכז ויצ",
+    "translation": "Parking",
+    category: 'car'
+  },
+  {
+    "keyword": "בולנז'רי 96",
+    "translation": "Backery",
+    category: 'food'
+  },
+  {
+    "keyword": "אייס סינרמה",
+    "translation": "ACE Home"
+  },
+  {
+    "keyword": "דקטלון",
+    "translation": "Decathlon",
+    category: 'sport'
+  },
+  {
+    "keyword": "קייזר",
+    "translation": "Mayson Kayser",
+    category: 'food'
+  },
+  {
+    "keyword": "עזריאלי גבעתיים",
+    "translation": "Azrieli Givataim"
+  },
+  {
+    "keyword": "OGNAY YB ILED",
+    "translation": "Deli",
+    category: 'food'
+  },
+  {
+    "keyword": "ILED OGNAY",
+    "translation": "Deli",
+    category: 'food'
+  },
+  {
+    "keyword": "סודקסו - סיבוס",
+    "translation": "Sibus over 800 ₪"
+  },
+  {
+    "keyword": "חשמל ר מ ז",
+    "translation": "Electrical shop"
+  },
+  {
+    "keyword": "מיני סנטר שרון",
+    "translation": "Hoz tovari"
+  },
+  {
+    "keyword": "פירות וירקות",
+    "translation": "Fruits and Vegetables",
+    category: 'food'
+  },
+  {
+    "keyword": "אלדו",
+    "translation": "Ice cream",
+    category: 'food'
+  },
+  {
+    "keyword": "חניון",
+    "translation": "Parking",
+    category: 'car'
+  },
+  {
+    "keyword": "מקס",
+    "translation": "Max"
+  },
+  {
+    "keyword": "קפה",
+    "translation": "Cafe",
+    category: 'food'
+  },
+  {
+    "keyword": "קופיקס",
+    "translation": "Cofix",
+    category: 'food'
+  },
+];
