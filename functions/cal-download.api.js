e5a9fc2f7322bff92bfd788c44471d4bab23c1bc
Vladi <v-ed@outlook.com>
2023-03-26 14:27:55 +0300

    updated functions

--- functions/cal-download.api.js
+++ functions/cal-download.api.js
@@ -1,6 +1,5 @@
 export const onRequestGet = async ({ request }) => {
   const calResp = await fetch("https://services.cal-online.co.il/Card-Holders/SCREENS/Transactions/Transactions.aspx", {
-    "credentials": "include",
     "headers": {
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0",
       "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
