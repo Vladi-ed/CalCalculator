f96dc01c0cfe16264113a4681cf24cb50392df48
Vladi <vladi@notifyai.io>
2023-05-09 19:22:56 +0300

    ng update, typescript 5 TODO: add github link (in a menu?), remove CloseButtonComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { NgModule, isDevMode } from '@angular/core';
+import {isDevMode, NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatInputModule} from "@angular/material/input";
 import {MatTableModule} from "@angular/material/table";
 import {MatSortModule} from "@angular/material/sort";
 import {MatRippleModule} from "@angular/material/core";
 import {NgxChartsModule} from "@swimlane/ngx-charts";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { CloseButtonComponent } from './components/close-button/close-button.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {CloseButtonComponent} from './components/close-button/close-button.component';
 
 @NgModule({
-  declarations: [AppComponent, CloseButtonComponent],
-  imports: [BrowserAnimationsModule, MatInputModule, MatTableModule, MatSortModule, MatRippleModule, NgxChartsModule, MatSlideToggleModule, ServiceWorkerModule.register('ngsw-worker.js', {
-  enabled: !isDevMode(),
-  // Register the ServiceWorker as soon as the application is stable
-  // or after 30 seconds (whichever comes first).
-  registrationStrategy: 'registerWhenStable:30000'
-})],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, CloseButtonComponent],
+    imports: [BrowserAnimationsModule, MatInputModule, MatTableModule, MatSortModule, MatRippleModule, NgxChartsModule, MatSlideToggleModule, ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
+    })],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
