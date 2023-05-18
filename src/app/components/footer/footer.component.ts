c99a76b66b963982170373b93bc6d81bfddeff8c
Vladi <vladi@notifyai.io>
2023-05-18 12:33:12 +0300

    made dialog styles global

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,11 +5,8 @@ import packageJson from 'package.json';
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styles: [
-      'dialog { border: none; border-radius: 4px; padding: 24px; font-weight: 400; box-shadow: 0 11px 15px -7px rgba(0, 0, 0, 0.2), 0 24px 38px 3px rgba(0, 0, 0, 0.14), 0 9px 46px 8px rgba(0, 0, 0, 0.12);}',
-      'dialog::backdrop { background: rgba(0,0,0,.32) }',
       'footer { text-align: center;  border-top: 1px solid lightgray; padding: 10px; font-size: small}',
       '.footer-link { text-decoration: underline; cursor: pointer; user-select: none }',
-      'h2 {text-align: center}'
   ]
 })
 export class FooterComponent {
