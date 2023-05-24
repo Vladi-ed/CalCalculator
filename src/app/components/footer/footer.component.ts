86e0d4d26a076ebbd2041a84d6aaad48ed598c89
Vladi <vladi@notifyai.io>
2023-05-24 19:14:47 +0300

    updated Help section, fixed styles

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,7 +5,7 @@ import packageJson from 'package.json';
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styles: [
-      'footer { text-align: center;  border-top: 1px solid lightgray; padding: 10px; font-size: small}',
+      'footer { text-align: center;  border-top: 1px solid lightgray; padding: 10px; font-size: small}' +
       '.footer-link { text-decoration: underline; cursor: pointer; user-select: none }',
   ]
 })
