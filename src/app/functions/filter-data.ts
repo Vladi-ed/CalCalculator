5c3148ee57ae89725784f1659e24be10f35a454e
Vladi <v-ed@outlook.com>
2023-05-28 01:03:47 +0300

    fixed 'other' category search

--- src/app/functions/filter-data.ts
+++ src/app/functions/filter-data.ts
@@ -8,7 +8,7 @@ import {ICalRecord} from "../interfaces/ICalRecord";
  */
 export function filterData(records: ICalRecord[], searchStr: string): ICalRecord[] {
 
-    if (searchStr === 'other') return records.filter(col => col.myCategory == undefined);
+    if (searchStr === 'other') return records.filter(col => col.myCategory == undefined || col.myCategory == 'other');
     else if (searchStr) {
       const sFilter = searchStr.toUpperCase();
       return records.filter(col => col.translation?.toUpperCase().includes(sFilter) ||
