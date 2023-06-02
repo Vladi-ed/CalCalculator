9849408d3d8f1b60f86c2fbddcf9f7f4b0583a1f
Vladi <v-ed@outlook.com>
2023-06-03 00:02:26 +0300

    moved styles to a separate file

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,10 +4,7 @@ import packageJson from 'package.json';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styles: [
-      'footer { text-align: center;  border-top: 1px solid lightgray; padding: 10px; font-size: small}' +
-      '.footer-link { text-decoration: underline; cursor: pointer; user-select: none }',
-  ]
+  styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
     readonly appVersion = packageJson.version;
