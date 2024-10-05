efde4b04354abf0c4133995b0b8bdd5a22726cf5
Vladi <v-ed@outlook.com>
2024-10-05 14:37:19 +0300

    removed unused dependency

--- functions/ai-translate.api.ts
+++ functions/ai-translate.api.ts
@@ -1,8 +1,5 @@
-// @ts-ignore
-import {Ai} from '@cloudflare/ai';
-
 interface Env {
-  AI: any
+  AI: Ai;
 }
 
 export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
@@ -12,13 +9,12 @@ export const onRequestGet: PagesFunction<Env> = async ({ request, env }) => {
 
   if (!text) return new Response('Missing data ' + requestParams.toString(), { status: 400, statusText: 'Missing data'});
 
-  const ai = new Ai(env.AI);
   const inputs = {
     text,
     source_lang: 'he',
     target_lang: 'en'
   };
-  const response = await ai.run('@cf/meta/m2m100-1.2b', inputs);
+  const response = await env.AI.run('@cf/meta/m2m100-1.2b', inputs);
 
   return Response.json({ inputs, response });
 }
