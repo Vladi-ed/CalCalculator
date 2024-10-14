a4adf256af6beb546e9e135bbf6576c79083d6e3
Vladi <v-ed@outlook.com>
2024-10-14 08:29:53 +0300

    replaced donate button

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
-import { version } from 'package.json';
+import { version } from '../../../../package.json';
+import { loadCustomScript } from "@paypal/paypal-js";
 
 @Component({
   selector: 'app-footer',
@@ -11,20 +12,22 @@ export class FooterComponent {
     readonly ngVersion = VERSION.full;
     protected readonly alert = alert;
 
-    ngOnInit() {
-        // @ts-ignore TODO: fix adding more that one button
+    async ngOnInit() {
+        try {
+            await loadCustomScript({ url: 'https://www.paypalobjects.com/donate/sdk/donate-sdk.js' });
+        } catch (error) {
+            console.error("failed to load the PayPal JS SDK script", error);
+        }
+
+        // @ts-ignore
         window.PayPal?.Donation.Button({
-            env:'production',
-            hosted_button_id:'EBY6KWECQY2D8',
+            env: 'production',
+            hosted_button_id: 'EBY6KWECQY2D8',
             image: {
-                src:'https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif',
-                alt:'Donate with PayPal button',
-                title:'Support my work',
+                src: 'assets/donate.png',
+                alt: 'Donate',
+                title: 'Support my work',
             }
         }).render('#donate-button');
     }
 }
-
-
-
-
